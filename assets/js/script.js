d63e52bb9e50cae0e4833a5e35d2f42a0a3308cb
Bobuskiy <bobuskiy.olexander@example.com>
2023-12-08 15:19:00 +0100

    Add changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
 "use strict";
 
 
-// $(".header-menu__button").click(function() {
-// 	$(".header-menu__mob").fadeIn(500);
-// 	$(".body").addClass('overflow');
-// });
-// $(".header-menu__close").click(function() {
-// 	$(".header-menu__mob").fadeOut(500);
-// 	$(".body").removeClass('overflow');
-// });
+$(".header-menu__button").click(function() {
+	$(".header-menu__mob").fadeIn(500);
+	$(".body").addClass('overflow');
+});
+$(".header-menu__close").click(function() {
+	$(".header-menu__mob").fadeOut(500);
+	$(".body").removeClass('overflow');
+});
+$(".menu-item").click(function() {
+	$(".header-menu__mob").fadeOut(500);
+	$(".body").removeClass('overflow');
+});
 
 document.addEventListener('DOMContentLoaded', function() {
 	const form = document.getElementById('form');
